26d5ad20240b37487348e96ca2026e6fcbc176f9
ryanshores <ryan.shores@me.com>
2024-08-16 17:48:50 -0500

    cleanup

--- src/__main__.py
+++ src/__main__.py
@@ -1,17 +1,10 @@
 import config
 import logging
 import writeScores
-from src.writeScores import logger
-
-LOGFILE_DIR = '../tmp/run.log'
 
 def main():
     config.configure_logger()
     logging.info('### START')
-    logging.error("error")
-    logger.debug("debug")
-    logger.warning("warn")
-    logger.fatal("fatal")
     writeScores.createCsv()
     logging.info('### END')
 
